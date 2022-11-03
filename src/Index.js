355d917c6bdd2b368975d9104188c0a8726be8f9
GC1920 <gcmenezes1@gmail.com>
2022-11-03 14:03:48 -0400

    Initial Commit

--- src/Index.js
+++ src/Index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 
 const app = express()
+const port = 3000
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
 
   res.send('Hello World')
-  
+
 })
 
-app.listen(3000)
+app.listen(port, () => {
+
+    console.log(`Servidor rodando em: http://localhost:${port}`)
+
+})
