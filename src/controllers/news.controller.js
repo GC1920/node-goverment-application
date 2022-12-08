6067c4ac0c04846ac8845902b9907037a23c2372
GC1920 <gcmenezes1@gmail.com>
2022-12-08 08:52:21 -0400

    Search news and express update

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -1,4 +1,4 @@
-import { createService, findAllService, countNewsService, topNewsService, findByIdService } from "../services/news.service.js";
+import { createService, findAllService, countNewsService, topNewsService, findByIdService, searchByTitleService } from "../services/news.service.js";
 
 export const create = async (req, res) => {
 
@@ -76,7 +76,7 @@ export const findAll = async (req, res) => {
                 name: item.user.name,
                 username: item.user.username,
                 userAvatar: item.user.avatar
-            }))
+            })),
         });
         
     } catch (err) {
@@ -146,3 +146,37 @@ export const findById = async (req, res) => {
         res.status(500).send({ message: err.message });
     }
 };
+
+export const searchByTitle = async (req, res) => {
+
+    try {
+
+        const { title } = req.query;
+
+        const news = await searchByTitleService(title);
+
+        if (news.length === 0) {
+
+            return res.status(400).send({ message: "There are no news with this title" });
+
+        }
+
+        return res.send({
+            results: news.map(item => ({
+                id: item._id,
+                title: item.title,
+                text: item.text,
+                banner: item.banner,
+                likes: item.likes,
+                comments: item.comments,
+                name: item.user.name,
+                username: item.user.username,
+                userAvatar: item.user.avatar
+            })),
+        });
+        
+    } catch (err) {
+        
+        res.status(500).send({ message: err.message });
+    }
+}
--- src/routes/news.routes.js
+++ src/routes/news.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 
-import { create, findAll, topNews, findById } from "../controllers/news.controller.js";
+import { create, findAll, topNews, findById, searchByTitle } from "../controllers/news.controller.js";
 import { authMiddleware } from "../middlewares/auth.middlewares.js";
 
 const router = Router();
@@ -8,6 +8,9 @@ const router = Router();
 router.post("/", authMiddleware, create);
 router.get("/", findAll);
 router.get("/top", topNews);
-router.get("/:id", findById)
+router.get("/search", searchByTitle);
+
+
+router.get("/:id", authMiddleware, findById);
 
 export default router
