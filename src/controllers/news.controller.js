34a55f898ac78edb39fd89fb5426c2040840a805
GC1920 <gcmenezes1@gmail.com>
2022-12-08 08:29:03 -0400

    Find by id news

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -1,6 +1,6 @@
-import { createService, findAllService, countNewsService, topNewsService } from "../services/news.service.js";
+import { createService, findAllService, countNewsService, topNewsService, findByIdService } from "../services/news.service.js";
 
-const create = async (req, res) => {
+export const create = async (req, res) => {
 
     try {
 
@@ -28,7 +28,7 @@ const create = async (req, res) => {
 
 };
 
-const findAll = async (req, res) => {
+export const findAll = async (req, res) => {
 
     try {
 
@@ -86,7 +86,7 @@ const findAll = async (req, res) => {
 
 };
 
-const topNews = async (req, res) => {
+export const topNews = async (req, res) => {
 
     try {
 
@@ -119,4 +119,30 @@ const topNews = async (req, res) => {
 
 };
 
-export { create, findAll, topNews }
+export const findById = async (req, res) => {
+
+    try {
+
+        const { id } = req.params;
+
+        const news = await findByIdService(id);
+
+        return res.send({
+            news: {
+                id: news._id,
+                title: news.title,
+                text: news.text,
+                banner: news.banner,
+                likes: news.likes,
+                comments: news.comments,
+                name: news.user.name,
+                username: news.user.username,
+                userAvatar: news.user.avatar
+            },
+        });
+        
+    } catch (err) {
+        
+        res.status(500).send({ message: err.message });
+    }
+};
--- src/routes/news.routes.js
+++ src/routes/news.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 
-import { create, findAll, topNews } from "../controllers/news.controller.js";
+import { create, findAll, topNews, findById } from "../controllers/news.controller.js";
 import { authMiddleware } from "../middlewares/auth.middlewares.js";
 
 const router = Router();
@@ -8,5 +8,6 @@ const router = Router();
 router.post("/", authMiddleware, create);
 router.get("/", findAll);
 router.get("/top", topNews);
+router.get("/:id", findById)
 
 export default router
